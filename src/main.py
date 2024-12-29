d46cc490b42c545edcac06eea131edb6762ed883
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2024-12-29 16:50:55 -0600

    Code cleanup, remove dependencies

--- src/main.py
+++ src/main.py
@@ -5,26 +5,6 @@ from dotenv import load_dotenv
 from src import logger
 from src.scrappers import text, diff, cars_com
 
-
-'''def main():
-    old_data = get_old_data()
-
-    url = ("https://www.cars.com/shopping/results/?dealer_id=&electric_total_range_miles_min=330&include_shippable"
-           "=true&keyword=&list_price_max=&list_price_min=&makes[]=hyundai&maximum_distance=all&mileage_max=&models["
-           "]=hyundai-ioniq_6&monthly_payment=&page_size=100&sort=listed_at_desc&stock_type=new&trims["
-           "]=hyundai-ioniq_6-se&year_max=&year_min=&zip=52241")
-    data = get_data(url)
-    cars = parse(data)
-
-    new_data = check_new_listings(old_data, cars)
-
-    if new_data.shape[0] > 0:
-        send_email(new_data)
-        print("Sending email...")
-
-    export(cars)'''
-
-
 if __name__ == '__main__':
     load_dotenv()
     sleep_time_sec = int(os.environ['SLEEP_TIME_SEC'])
