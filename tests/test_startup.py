91a31e26a3fdafe33cf1442024536bde76d3e3c7
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-03-09 18:30:06 -0500

    Update test and upload coverage and test report

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -1,6 +1,9 @@
 from unittest import TestCase
 
+from main import main
+
 
 class TestStartup(TestCase):
-    def test_health_check(self):
+    def test_startup(self):
+        main()
         assert True
