d0dcedd776956e3894c26f0037b6eade5550d21a
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-09-30 18:05:59 -0500

    Optimize git fetch and merge in update.sh
    Update the update.sh script to use shallow fetch and allow unrelated histories.

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,10 +2,10 @@
 
 # Set these variables
 PYTHON_STARTER_URL="https://github.com/jnstockley/python-starter.git"
-PYTHON_STARTER_BRANCH="main"  # or the branch you want to merge
+PYTHON_STARTER_BRANCH="main" # or the branch you want to merge
 
 # Pull changes from python starter repo into current git repo
 git remote add pythonStarter "$PYTHON_STARTER_URL"
-git fetch pythonStarter
-git merge pythonStarter/"$PYTHON_STARTER_BRANCH"
-git remote remove PYTHON_STARTER_URL
+git fetch pythonStarter --depth=1
+git merge --allow-unrelated-histories --no-edit pythonStarter/"$PYTHON_STARTER_BRANCH"
+git remote remove pythonStarter
