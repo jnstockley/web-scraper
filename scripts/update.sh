17a8b31bd4af6197faae2c513eeb30376af3a20a
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2025-09-30 08:33:16 -0500

    Create script to update repos that use this template
    Updated the script to pull changes from a Python starter repository instead of repo A.

---
+++ scripts/update.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Set these variables
+PYTHON_STARTER_URL="https://github.com/jnstockley/python-starter.git"
+PYTHON_STARTER_BRANCH="main"  # or the branch you want to merge
+
+# Pull changes from python starter repo into current git repo
+git remote add pythonStarter "$PYTHON_STARTER_URL"
+git fetch pythonStarter
+git merge pythonStarter/"$PYTHON_STARTER_BRANCH"
+git remote remove PYTHON_STARTER_URL
